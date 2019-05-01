5f1a72bf8f0580422a9a8bf5ddfaedb27fcacc00
Igor Iakymenko <iia@ciklum.com>
2019-05-02 01:00:30 +0300

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get(searchText: string) {
+    let params = new HttpParams();
+
+    params = params.append('api_key', '8fa254190ec943ff6dd2558a1dbbbe74');
+    params = params.append('text', searchText);
+    params = params.append('extras', 'url_m');
+    params = params.append('format', 'json');
+    params = params.append('nojsoncallback', '1');
+    params = params.append('method', 'flickr.photos.search');
+
+    return this.http.get('https://api.flickr.com/services/rest', { params });
+  }
+}
